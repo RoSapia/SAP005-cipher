43aa99bede48f70c1152d5bccf18980def8380cf
RoSapia <roberta.sapia@hotmail.com>
2020-10-28 11:34:51 -0300

    add select e option

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import cipher from './cipher.js';
 
-var num = document.getElementById("offset")
-function offset(num){
-    var select = document.getElementsByTagName('select')[0];
+
+function offset(){
+    var offset = document.getElementById('offset');
+    var c_select = document.createElement('select');
+    offset.appendChild(c_select);
+    var i = 0;
         for (i = 1; i < 26; i++) {
           var newoption = document.createElement('option');
-          select.appendChild(newoption);
-          newoption.innerHTML = i;
-          return num;
+          c_select.appendChild(newoption);    
+          newoption.innerHTML = i;          
         }
 }
 
+offset();
+
 console.log(cipher);
+
