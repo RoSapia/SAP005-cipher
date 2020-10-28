a3ac77d9f1b11dc1c4a5b134e5b89cd08e35d824
RoSapia <roberta.sapia@hotmail.com>
2020-10-28 10:44:49 -0300

    add função offset

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import cipher from './cipher.js';
 
+var num = document.getElementById("offset")
+function offset(num){
+    var select = document.getElementsByTagName('select')[0];
+        for (i = 1; i < 26; i++) {
+          var newoption = document.createElement('option');
+          select.appendChild(newoption);
+          newoption.innerHTML = i;
+          return num;
+        }
+}
+
 console.log(cipher);
