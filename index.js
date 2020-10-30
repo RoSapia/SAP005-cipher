e365314659eaa99e103971140228a380580dc83d
RoSapia <roberta.sapia@hotmail.com>
2020-10-30 17:26:24 -0300

    mudança de var para let

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import cipher from './cipher.js';
 
 
 function offset(){
-    var offset = document.getElementById('offset');
-    var c_select = document.createElement('select');
+    let offset = document.getElementById('offset');
+    let c_select = document.createElement('select');
     offset.appendChild(c_select);
-    var i = 0;
-        for (i = 1; i < 26; i++) {
-          var newoption = document.createElement('option');
+    let i = 0;
+        for (i = 1; i <= 26; i++) {
+          let newoption = document.createElement('option');
           c_select.appendChild(newoption);    
           newoption.innerHTML = i;          
         }
