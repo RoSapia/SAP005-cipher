675d4104ab8da4fd91f253a31920ff2ebe547025
RoSapia <roberta.sapia@hotmail.com>
2020-11-03 20:56:37 -0300

    implementação de encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,17 +1,35 @@
-const cipher = {
+const cipher = {//pq
   // ...
 };
 
-export default cipher;
+export default cipher;//pq
 
-function encode(offset, strCipher) {
-  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-  offset = document.getElementById(offset);
-  strCipher = document.getElementById(strCipher).toUpperCase();
-  let newStrCipher = '';
-  for (let i = 0; i < strCipher.length; i++) {
-    
+function encode(offset, strCipher){
+  offset = Number(document.getElementById(offset));
+  strCipher = document.getElementById(strCipher).value.toUpperCase();
+  let newStr = '';
+  for(let i=0; i < strCipher.length; i++){
+    let cod = strCipher.charCodeAt(i);
+    let newCode = ((cod - 65 + offset)% 25 + 65);
+    newStr = String.fromCharCode(newCode);
   }
 
+
 }
-encode();
+
+
+/*function encode(offset, strCipher) {
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  offset = Number(document.getElementById(offset));
+  strCipher = document.getElementById(strCipher).value.toUpperCase();
+  let newStrCipher = '';
+  for (let i = 0; i < strCipher.length; i++) {
+    let strLetter = strCipher[i];
+    if(strLetter === ' '){
+      newStrCipher += strLetter;
+      continue;
+    }
+    let 
+  }
+}*/
+  //
