959b70289b648a081907a6e9b4b8338990acb2ba
RoSapia <roberta.sapia@hotmail.com>
2020-11-03 10:54:57 -0300

    selecionando a escolha

--- cipher.js
+++ cipher.js
@@ -3,3 +3,11 @@ const cipher = {
 };
 
 export default cipher;
+
+function encode(offset){
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let alphabet_array = alphabet.split("") 
+
+  console.log(alphabet_array);
+
+}
