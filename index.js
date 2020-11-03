189c29960bfd27f9d2a6910dc75c03f355b2f1de
RoSapia <roberta.sapia@hotmail.com>
2020-11-03 20:56:37 -0300

    implementação de encode

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-import cipher from './cipher.js';
+import cipher from './cipher.js';//pq
 
 
 function offset() {
   let offset = document.getElementById('offset');
   let c_select = document.createElement('select');
   offset.appendChild(c_select);
-  //let i = 0;
   for (let i = 1; i <= 26; i++) {
     let newoption = document.createElement('option');
     c_select.appendChild(newoption);
     newoption.innerHTML = i;
   }
 }
-
-function message_choice() {
+offset();
+/*function message_choice() {
   let button_submit = document.getElementById(submit)
   let button_encode = document.getElementById(encode);
   let button_decode = document.getElementById(decode);
-  button_submit.addEventListener("click", function() {
-  if (button_encode.addEventListener("click", encode, true)) {//se tiver clicado chama a função encode
-    console.log("Botão encode clicado ");
+  button_submit.addEventListener("click", function () {
+    if (button_encode.addEventListener("click", encode, true)) {//se tiver clicado chama a função encode
+      console.log("Botão encode clicado ");
+    }
+    else if (button_decode.addEventListener("click"))
   }
-  else if(button_decode.addEventListener("click"))
-}}
+}*/
 
-offset();
-radio_choice();
-console.log(cipher);
+//offset();
+//radio_choice();
+console.log(cipher);//pq
 
