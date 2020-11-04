0e91e3b75fc6ad600978491ffda285db6bd8182b
RoSapia <roberta.sapia@hotmail.com>
2020-11-04 15:18:30 -0300

    add funcionalidade radio

--- cipher.js
+++ cipher.js
@@ -1,36 +1,16 @@
 const cipher = {
-   encode(offset, strCipher){
-    //offset = Number(document.getElementById("offset"));
-    //strCipher = document.getElementById("strCipher").value.toUpperCase();
+  encode(offset, strCipher) {
     let newStr = '';
-    for(let i=0; i < strCipher.length; i++){
+    for (let i = 0; i < strCipher.length; i++) {
       let cod = strCipher.charCodeAt(i);
       let newCode = ((cod - 65 + offset) % 25 + 65);
       newStr += String.fromCharCode(newCode);
-      }
-    //document.getElementById("strCipher").value = newStr;  
+    }
     return newStr;
-  
+
   }
-  
-  
+
+
 };
 
-export default cipher;//pq
-
-
-/*function encode(offset, strCipher) {
-  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-  offset = Number(document.getElementById(offset));
-  strCipher = document.getElementById(strCipher).value.toUpperCase();
-  let newStrCipher = '';
-  for (let i = 0; i < strCipher.length; i++) {
-    let strLetter = strCipher[i];
-    if(strLetter === ' '){
-      newStrCipher += strLetter;
-      continue;
-    }
-    let 
-  }
-}*/
-  //
+export default cipher;
