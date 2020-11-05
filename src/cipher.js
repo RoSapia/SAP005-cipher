5ad536dcf06aa50fab603c2e5348bb10bdcab8b5
RoSapia <roberta.sapia@hotmail.com>
2020-11-04 23:32:42 -0300

    add função decode

--- src/cipher.js
+++ src/cipher.js
@@ -3,14 +3,31 @@ const cipher = {
     let newStr = '';
     for (let i = 0; i < strCipher.length; i++) {
       let cod = strCipher.charCodeAt(i);
-      let newCode = ((cod - 65 + offset) % 25 + 65);
-      newStr += String.fromCharCode(newCode);
+      if (cod >= 65 && cod <= 90) {
+        let newCode = ((cod - 65 + offset) % 26 + 65);
+        newStr += String.fromCharCode(newCode);
+      }
+      else {
+        newStr += String.fromCharCode(cod);
+      }
     }
     return newStr;
-
+  },
+  
+  decode(offset, strCipher) {
+    let newStr = '';
+    for (let i = 0; i < strCipher.length; i++) {
+      let cod = strCipher.charCodeAt(i);
+      if (cod >= 65 && cod <= 90) {
+        let newCode = ((cod - 90 - offset) % 26 + 90);
+        newStr += String.fromCharCode(newCode);
+      }
+      else {
+        newStr += String.fromCharCode(cod);
+      }
+    }
+    return newStr;
   }
-
-
 };
 
 export default cipher;
