c2808b34991c12e14a2c6d1786bcbf4535455111
RoSapia <roberta.sapia@hotmail.com>
2020-11-07 10:45:23 -0300

    organização da indentação

--- src/cipher.js
+++ src/cipher.js
@@ -1,32 +1,32 @@
 const cipher = {
   encode(offset, strCipher) {
-    let newStr = '';
+    let newStr = ''
     for (let i = 0; i < strCipher.length; i++) {
-      let cod = strCipher.charCodeAt(i);
+      let cod = strCipher.charCodeAt(i)
       if (cod >= 65 && cod <= 90) {
-        let newCode = ((cod - 65 + offset) % 26 + 65);
-        newStr += String.fromCharCode(newCode);
+        let newCode = ((cod - 65 + offset) % 26 + 65)
+        newStr += String.fromCharCode(newCode)
       }
       else {
-        newStr += String.fromCharCode(cod);
+        newStr += String.fromCharCode(cod)
       }
     }
-    return newStr;
+    return newStr
   },
 
   decode(offset, strCipher) {
-    let newStr = '';
+    let newStr = ''
     for (let i = 0; i < strCipher.length; i++) {
-      let cod = strCipher.charCodeAt(i);
+      let cod = strCipher.charCodeAt(i)
       if (cod >= 65 && cod <= 90) {
-        let newCode = ((cod - 90 - offset) % 26 + 90);
-        newStr += String.fromCharCode(newCode);
+        let newCode = ((cod - 90 - offset) % 26 + 90)
+        newStr += String.fromCharCode(newCode)
       }
       else {
-        newStr += String.fromCharCode(cod);
+        newStr += String.fromCharCode(cod)
       }
     }
-    return newStr;
+    return newStr
   }
 };
 
