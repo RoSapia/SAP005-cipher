b060aef14c92b6f6e32f48aea61386cdcf3321e0
RoSapia <roberta.sapia@hotmail.com>
2020-11-07 15:42:31 -0300

    css

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import cipher from './cipher.js';
 
-let offsetDiv = document.getElementById("offsetDiv")
-let offset = document.createElement("select")
-offsetDiv.appendChild(offset)
+let offsetDiv = document.getElementById("offsetDiv");
+let offset = document.createElement("select");
+offsetDiv.appendChild(offset);
 for (let i = 1; i < 26; i++) {
-  let newoption = document.createElement("option")
-  offset.appendChild(newoption)
-  newoption.innerHTML = i
-  newoption.value = i
+  let newoption = document.createElement("option");
+  offset.appendChild(newoption);
+  newoption.innerHTML = i;
+  newoption.value = i;
 }
 
-let buttonSubmit = document.getElementById("submit")
-let choice = document.getElementsByName("choice")
+let buttonSubmit = document.getElementById("submit");
+let choice = document.getElementsByName("choice");
 buttonSubmit.addEventListener("click", function (evt) {
-  evt.preventDefault()
-  let strCipher = document.getElementById("str_cipher").value
+  evt.preventDefault();
+  let strCipher = document.getElementById("str_cipher").value;
 
   if (choice[0].checked) {
-    document.getElementById("str_cipher").value = cipher.encode(Number(offset.value), strCipher.toString().toUpperCase())
+    document.getElementById("str_cipher").value = cipher.encode(Number(offset.value), strCipher.toString().toUpperCase());
   }
   else if (choice[1].checked) {
-    document.getElementById("str_cipher").value = cipher.decode(Number(offset.value), strCipher.toString().toUpperCase())
+    document.getElementById("str_cipher").value = cipher.decode(Number(offset.value), strCipher.toString().toUpperCase());
   }
 });
 
